aafb44285bbb1dc0ff5c30f3f57cc446b7f36ff4
Christopher Arndt <chris@chrisarndt.de>
2017-12-13 17:41:24 +0100

    Fix twine upload command line
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- release.sh
+++ release.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 VERSION="$(grep 'version=' setup.py | cut -d \' -f 2)"
+SDIST="dist/picoredis-$VERSION.tar.gz"
+WHEEL="dist/picoredis-$VERSION-py2.py3-none-any.whl"
 
 pandoc --from markdown --to rst README.md > README.rst
 python setup.py sdist --format=gztar
 python setup.py bdist_wheel
 ls -l dist
-tar -tzf dist/picoredis-$VERSION.tar.gz
+tar -tzf "$SDIST"
 
 if [ "x$1" = "x-u" ]; then
-    twine upload -r pypi dist
+    twine upload -r pypi "$SDIST" "$WHEEL"
 fi
